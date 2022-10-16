c1b882de382af7b9d6d8d7763b3da100dbf7b6d2
Mohamed Bentouhami Belhaj <56387@etu.he2b.be>
2022-10-16 23:27:37 +0200

    add forms

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 
-from listings.forms import ContactUsForm
+from listings.forms import ContactUsForm,BandForm,ListingForm
 from listings.models import Band, Listing
 
 def band_list(request):
@@ -13,6 +13,18 @@ def band_detail(request,id):
     band = Band.objects.get(id=id)
     return render(request,'groups/band_detail.html',{'band':band})
 
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail',band.id)
+    else:
+        form = BandForm()
+
+
+    return render(request,'groups/band_create.html',{'form':form})
+
 def about(request):
     return render(request,'about.html')
 
@@ -22,6 +34,17 @@ def listing(request):
 def listing_detail(request,id):
     listing = Listing.objects.get(id=id)
     return render(request,'listings/listing_detail.html',{'listing':listing})
+
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail',listing.id)
+    else :
+        form = ListingForm()
+
+    return render(request,'listings/listing_create.html',{'form':form})
 
 def contact(request):
 
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -21,8 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bands/',views.band_list, name='band-list'),
     path('bands/<int:id>/',views.band_detail, name='band-detail'),
+    path('band/add',views.band_create,name="band-create"),
     path('listings/',views.listing, name='listing-list'),
     path('listings/<int:id>/',views.listing_detail, name='listing-detail'),
+    path('listings/add/',views.listing_create, name='listing-create'),
     path('about-us/',views.about,name="about"),
     path('contact-us/',views.contact,name = 'contact'),
     path('email-send/',views.email_send,name = "email-send")
